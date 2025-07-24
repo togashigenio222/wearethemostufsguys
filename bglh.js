452e5f99abda4706315de0853fec3a02a1cd6e94
Daví Ribeiro <daviribemarbeiro@gmail.com>
2025-07-24 20:02:17 -0300

    Update bglh.js

--- bglh.js
+++ bglh.js
@@ -1,37 +1,34 @@
+//1
 const alunos = [
-    {aluno: 'a' , idade: 19, matricula: 209809210, curso: 'si'},
-    {aluno: 'b' , idade: 19, matricula: 20921, curso: 'cc'},
-    {aluno: 'c' , idade: 20, matricula: 209009210, curso: 'ec'},
-    {aluno: 'd' , idade: 25, matricula: 209509210, curso: 'ec'},
-    {aluno: 'e' , idade: 50, matricula: 209509210, curso: 'ec'}
+    {aluno: 'Rhuan' , idade: 18, matricula: 202500035548, curso: 'SI'},
+    {aluno: 'Pablo' , idade: 20, matricula: 202400045768, curso: 'CC'},
+    {aluno: 'José' , idade: 31, matricula: 201900088976, curso: 'SI'},
+    {aluno: 'Antônio' , idade: 29, matricula: 202100099876, curso: 'EC'},
+    {aluno: 'Daví' , idade: 19, matricula: 202200078792, curso: 'CC'}
 
 ]
-
+//2
 const adicionarAluno = (nomeRe,idadeRe,matriculaRe,cursoRe) => [
-    {aluno: 'a', idade: 19, matricula: 209209210, curso: 'ec'}]
-
+    {aluno: 'Arlindo', idade: 19, matricula: 202500032509, curso: 'EC'}]
+//3
 const turma = [
     ...alunos, adicionarAluno
     ]
+    //4
 const listarAlunos = (discente) => alunos.map((x)=> x.aluno == discente? x: null)
-
+//5
 const buscarCurso = (curso) => alunos.map((x)=> x.curso == curso? x: null)
-
+//6
 const remover = (nome) => alunos.filter((x) => x.aluno !== nome)
+//7
+const ordenarMatricula = alunos.sort((a, b)=> a.matricula - b.matricula)
 
-const mtr = alunos.sort((a, b)=> a.matricula - b.matricula)
-
-// const numcur = (lista,nome) => {
-//     const contagem = lista.filter((x) => x.curso == aluos.filter
-// const numcur = alunos.filter((x) => x.curso == alunos.filter((d)=> ))
 const numcur = (lista,nome) => {
     const contagem = lista.filter((x) => x.curso == nome)
-    return contagem.lenght()
+    return contagem.length
 }
-// console.log(numcur(alunos,'cc'))
-const modificar = alunos.filter((a) => a.matricula == 20921)
-const lista = [...modificar, modificar.aluno = 'José']
-console.log(lista)
+console.log(numcur(alunos,'CC'))
+
 
 
 
